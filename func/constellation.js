95ad738d454687d614dedb1dffa4f2d0447e08eb
Cliff Su <stu01509@yahoo.com.tw>
2018-11-11 09:32:08 +0800

    🆕 Update Files and Fix Bug

--- func/constellation.js
+++ func/constellation.js
@@ -1,5 +1,3 @@
 const btdc = require('bottender-compose');
 
-module.exports = btdc.series([
-  btdc.sendText('水瓶座\n雙魚座\n牡羊座 / 白羊座\n金牛座\n雙子座\n巨蟹座\n獅子座\n處女座\n天秤座\n天蠍座\n射手座 / 人馬座\n魔羯座 / 山羊座'),
-]);
+module.exports = btdc.sendText('水瓶座\n雙魚座\n牡羊座 / 白羊座\n金牛座\n雙子座\n巨蟹座\n獅子座\n處女座\n天秤座\n天蠍座\n射手座 / 人馬座\n魔羯座 / 山羊座');
